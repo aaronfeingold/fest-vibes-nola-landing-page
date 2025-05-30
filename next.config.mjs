dc22d5afbbc5dcc74436bd4eafe0a0d187701b7b
aaron.feingold <ajfeingold88@gmail.com>
2025-05-29 23:11:47 -0500

    build fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: "out",
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +13,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
 export default nextConfig
